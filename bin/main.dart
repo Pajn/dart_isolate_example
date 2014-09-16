dd13e36db734fe04b558eb8b9ca0df21eb05731c
Rasmus Eneman <rasmus@eneman.eu>
2014-09-16 12:52:32 +0200

    Test two threads

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 import 'dart:isolate';
 
@@ -9,9 +10,12 @@ main(List<String> args) {
     var userMessage = args.join(' ');
 
     var receivePort = new ReceivePort();
+    var receivePort2 = new ReceivePort();
     var uri = new Uri.file('../lib/isolate.dart');
-    Isolate.spawnUri(uri, [], receivePort.sendPort);
+    Isolate.spawnUri(uri, [1], receivePort.sendPort);
+    Isolate.spawnUri(uri, [2], receivePort2.sendPort);
     SendPort sendPort;
+    SendPort sendPort2;
 
     receivePort.listen((message) {
         if (sendPort == null && message is SendPort) {
@@ -19,7 +23,23 @@ main(List<String> args) {
             sendPort.send(userMessage);
         } else if (message is String) {
             print(message);
-            exit(0);
         }
     });
+
+    receivePort2.listen((message) {
+        if (sendPort2 == null && message is SendPort) {
+            sendPort2 = message;
+            sendPort2.send(userMessage);
+        } else if (message is String) {
+            print(message);
+        }
+    });
+
+    print('from main thread');
+
+    new Timer.periodic(const Duration(seconds: 1), (_) {
+        print('Hello from the main Isolate');
+    });
+
+    print('last in main thread');
 }
--- lib/isolate.dart
+++ lib/isolate.dart
@@ -1,14 +1,31 @@
+import 'dart:async';
 import 'dart:isolate';
+import 'dart:io';
 
-main(_, SendPort sendPort) {
+main(args, SendPort sendPort) {
     var receivePort = new ReceivePort();
 
     receivePort.listen((String message) {
         if (message is String) {
             var reversedMessage = message.split('').reversed.join('');
-            sendPort.send(reversedMessage);
+            sendPort.send(reversedMessage + ' ${args.first}');
         }
     });
 
     sendPort.send(receivePort.sendPort);
+
+    new Timer.periodic(const Duration(seconds: 1), (_) {
+        print('Hello Isolate ${args.first}');
+    });
+
+    if (args.first != 12) {
+        var start = new DateTime.now();
+        var num = 1.01276452783498;
+        for (int i = 0; i < 1000000000; i++) {
+            num *= num;
+        }
+        print(num);
+        print(new DateTime.now().difference(start));
+    }
+
 }
